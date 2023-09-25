6d011fc1182ffe62746ab08026cc12192ddd96e9
shiju <shiju1283@163.com>
2023-09-25 22:06:11 +0800

    Revert "项目初始化"
    This reverts commit d360a4d86e1f8cab8e35452643d4b9d99cc236f3.

--- common/lib/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	
-    baseUrl:"http://127.0.0.1:8200/app",
-	version: "/v1",
-	
-}
--- common/mixin/base.js
+++
@@ -1,8 +0,0 @@
-import $Time from "@/common/lib/time.js"
-export default {
-	filters: {
-		formatTime(value) {
-			return $Time.gettime(value);
-		}
-	},
-}
